a9582eb53c3d82a6cebdaefcc041976d3c1bef53
KseniaBP <ksenia.dee@mail.ru>
2023-08-08 04:27:32 +0300

    Task02 completed

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -12,9 +12,15 @@ Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
+    Console.Write("max = "); 
     Console.WriteLine(number1);
+    Console.Write("min = "); 
+    Console.Write(number2);
 }
 else
 {
+    Console.Write("max = "); 
     Console.WriteLine(number2);
+    Console.Write("min = "); 
+    Console.Write(number1);
 }
