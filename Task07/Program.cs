0ffa10743d7dc90ff62d958ac2c1060975afdc0e
KseniaBP <ksenia.dee@mail.ru>
2023-08-08 06:05:42 +0300

    Task07 added

---
+++ Task07/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая принимает на вход трехзначное число 
+и на выходе показывает последнюю цифру этого числа.
+*/
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    Console.WriteLine(number%10);
+}
+else 
+{
+    Console.WriteLine("Введено некорректное число");
+}
