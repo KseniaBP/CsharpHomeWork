a462ba646897b330f28b0b28849e51c1bdaaa777
KseniaBP <ksenia.dee@mail.ru>
2023-08-29 07:31:01 +0300

    Task18 added

---
+++ Task18/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат в этой четверти
+*/
+
+Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Координаты оси X > 0. Координаты оси Y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Координаты оси X < 0. Координаты оси Y > 0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Координаты оси X < 0. Координаты оси Y < 0");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Координаты оси X > 0. Координаты оси Y < 0");
+}
+else
+{
+    Console.WriteLine("Нет такой четверти");
+}
