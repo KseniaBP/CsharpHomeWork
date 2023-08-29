fa5018d70190fe0e8586c06ba262fd20882ed5c0
KseniaBP <ksenia.dee@mail.ru>
2023-08-29 18:06:57 +0300

    Task26 added

---
+++ Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+*/
+
+Console.WriteLine("Введите число:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    return count;
+}
+Console.WriteLine ($"В числе {userNumber} {Quantity(userNumber)} цифр(а/ы)");
