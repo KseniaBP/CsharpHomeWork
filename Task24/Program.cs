008ee91c1097b1220fdfe382a92d8ce95bef06b7
KseniaBP <ksenia.dee@mail.ru>
2023-08-29 17:01:29 +0300

    Task24 added

---
+++ Task24/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход число A и выдает сумму числел от 1 до A
+*/
+
+Console.WriteLine("Введите число:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+/*int sum = 0;
+
+for (int i = 1; i <= userNumber; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine(sum);*/
+
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= userNumber; i++)
+{
+    sum += i;
+}
+    return sum;
+}
+Console.WriteLine($"сумма чисел от 1 до {userNumber} равна {SumOfNumber(userNumber)}");
