ad78fa379fa8e0683a1ae275456c4f24e7e8e956
KseniaBP <ksenia.dee@mail.ru>
2023-09-15 00:52:01 +0300

    Task44 added

---
+++ Task44/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
+
+int[] FillFibArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    Console.Write("Введите первый элемент массива: ");
+    array[0] = Convert.ToInt32(Console.ReadLine());
+    array[1] = array[0] + 1;
+
+    for (int i = 2; i < size; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+
+    return array;
+}
+void PrintArray(int[] arrayForPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        Console.Write(arrayForPrint[i]);
+        if (i < arrayForPrint.Length - 1) System.Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+int[] fibArray = FillFibArray();
+PrintArray(fibArray);
