fd65a637f1ce9c66cd334054a62905d680653f78
KseniaBP <ksenia.dee@mail.ru>
2023-09-15 09:24:26 +0300

    Task47 added

---
+++ Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+double[,] FillArray()
+{
+    Console.Write("Задайте количествово столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Задайте количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[rows, columns];
+    Console.Write("Введите минимальное значение: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное значение: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * (max - min) + (min);
+        }
+    }
+
+    return array;
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(array[i, j], 2) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(FillArray());
