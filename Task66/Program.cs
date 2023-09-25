93460a2a4a6fdc061140a80e90ed5202f60f88fa
KseniaBP <ksenia.dee@mail.ru>
2023-09-25 19:31:10 +0300

    Task66 added

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите первое число: ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumMToN(userNumberM, userNumberN);
+Console.WriteLine(sum);
+
+int SumMToN(int numberM, int numberN)
+{
+    if (numberM == numberN) return numberM;
+    else return numberM + SumMToN(numberM + 1, numberN);
+}
